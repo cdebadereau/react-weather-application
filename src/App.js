4b4bc80326095859c7bf05bdb3058d6faeeda54b
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2022-05-08 19:24:17 +0200

    Added the html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           <a
             href="https://github.com/cdebadereau/react-weather-application"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             Open-sourced code
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              autoFocus
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Florence</h1>
+      <ul>
+        <li>Sunday, 18:00</li>
+        <li>Partially cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partially cloudy"
+          />
+          19°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 64%</li>
+            <li>Wind: 11km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
